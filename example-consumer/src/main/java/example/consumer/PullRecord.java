f12480e867269e09f954a4ac34c7101a67e47b33
qiangjin <qiangjin613@163.com>
2023-01-19 23:16:24 +0800

    feat(Producer): 添加 kafka Consumer 模块
    1. 添加消费者示例代码；
    2. 修改生产者 SendRecord.java 中的 KafkaProducer close() 为 try-whit-resource；
    3. 添加 log4j 日志框架。
    
    提交人：qiangj

---
+++ example-consumer/src/main/java/example/consumer/PullRecord.java
@@ -0,0 +1,50 @@
+package example.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+/**
+ * 拉取消息
+ * <p>
+ *
+ */
+public class PullRecord {
+    public static void main(String[] args) {
+        // 填充配置
+        Properties properties = new Properties();
+
+        // 连接集群
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.100:9092");
+        // 指定 k v 的反序列化类型
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // 指定 Consumer Group ID
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "t");
+
+        // 创建 Kafka Consumer 对象
+        try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties)) {
+            // 订阅主题
+            kafkaConsumer.subscribe(Collections.singletonList("topicA"));
+            // 拉取数据
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(1));
+                for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                    // 简单地打印对象
+                    System.out.println(consumerRecord);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- example-producer/src/main/java/example/producer/SendRecord.java
+++ example-producer/src/main/java/example/producer/SendRecord.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 /**
- * 异步发送
+ * 发送消息
  * <p>
  *  1. 异步发送、异步发送带回调方法
  *  2. 同步发送
@@ -18,35 +18,47 @@ public class SendRecord {
         // 添加配置
         Properties properties = new Properties();
         // 连接集群
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.100:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.101:9092");
         // 指定 k v 的序列化类型
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // 批次大小
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        // 等待时间
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, 0);
+        // 数据压缩
+        properties.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "none");
+        // 缓冲区大小
+        properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+
+        // 应答级别
+        properties.put(ProducerConfig.ACKS_CONFIG, "-1");
+        // 重新发送次数
+        properties.put(ProducerConfig.RETRIES_CONFIG, 5);
+
         // 创建kafka producer 对象
-        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
-        // 异步发送数据
-        for (int i = 0; i < 10; i++) {
-            Future<RecordMetadata> metadataFuture = kafkaProducer.send(new ProducerRecord<>("topicA", "hello Kafka " + i));
-        }
-        // 异步发送数据 - callback
-        for (int i = 0; i < 10; i++) {
-            Future<RecordMetadata> metadataFuture = kafkaProducer.send(new ProducerRecord<>("topicA", "hello Kafka " + i), (metadata, exception) -> {
-                if (exception == null) {
-                    System.out.println("主题：" + metadata.topic() + "，分区：" + metadata.partition());
+        try (KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties)) {
+            // 异步发送数据
+            for (int i = 0; i < 10; i++) {
+                Future<RecordMetadata> metadataFuture = kafkaProducer.send(new ProducerRecord<>("topicA", "hello Kafka " + i));
+            }
+            // 异步发送数据 - callback
+            for (int i = 0; i < 10; i++) {
+                Future<RecordMetadata> metadataFuture = kafkaProducer.send(new ProducerRecord<>("topicA", "hello Kafka " + i), (metadata, exception) -> {
+                    if (exception == null) {
+                        System.out.println("主题：" + metadata.topic() + "，分区：" + metadata.partition());
+                    }
+                });
+            }
+            // 同步发送数据
+            for (int i = 0; i < 10; i++) {
+                try {
+                    RecordMetadata metadata = kafkaProducer.send(new ProducerRecord<>("topicA", "hello Kafka " + i)).get();
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
                 }
-            });
-        }
-        // 同步发送数据
-        for (int i = 0; i < 10; i++) {
-            try {
-                RecordMetadata metadata = kafkaProducer.send(new ProducerRecord<>("topicA", "hello Kafka " + i)).get();
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException(e);
             }
         }
-
-        // 关闭资源
-        kafkaProducer.close();
     }
 }
